d5830b86841b15ea2db26cb34e00ba2d4667d4fa
Vlad <v.ch@newizze.com>
2020-10-12 15:39:05 +0300

    Edit test Schedule create

--- src/test/java/ru/iCRM/TestSchedule.java
+++ src/test/java/ru/iCRM/TestSchedule.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestSchedule extends WebDriverSettings {
@@ -33,11 +34,11 @@ public class TestSchedule extends WebDriverSettings {
             driver.findElementByName("clinic_type_id").click();
             driver.findElementByCssSelector("#app > div > div.panel-body > div > div > form > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div > select > option:nth-child(2)").click();
             driver.findElementByLinkText("Select an Option").click();
-            driver.findElementByCssSelector("#app > div > div.panel-body > div > div > form > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div > div > div > ul > li:nth-child(6)").click();
+            driver.findElementByXPath("//*[@id=\"app\"]/div/div[2]/div/div/form/div[2]/div[1]/div[2]/div/div/div/ul/li[2]").click();
             Thread.sleep(2000);
             driver.findElementByName("date").click();
             Thread.sleep(1000);
-            WebElement datetoday = driver.findElementByCssSelector("#ui-datepicker-div > table > tbody > tr:nth-child(1) > td.ui-datepicker-days-cell-over.ui-datepicker-today");
+            WebElement datetoday = driver.findElementByXPath("//*[@id=\"ui-datepicker-div\"]/table/tbody/tr[3]/td[2]/a");
             datetoday.click();
             driver.findElementByName("start_time").click();
             driver.findElementByCssSelector("#app > div > div.panel-body > div > div > form > div:nth-child(3) > div:nth-child(1) > div:nth-child(4) > div > select > option:nth-child(2)").click();
@@ -45,17 +46,17 @@ public class TestSchedule extends WebDriverSettings {
             driver.findElementByCssSelector("#app > div > div.panel-body > div > div > form > div:nth-child(3) > div:nth-child(1) > div:nth-child(5) > div > select > option:nth-child(10)").click();
             driver.findElementByName("anaesthetists").click();
             driver.findElementByCssSelector("#app > div > div.panel-body > div > div > form > div:nth-child(3) > div:nth-child(2) > div.form-group.col-lg-12 > div > select > option:nth-child(2)").click();
-            driver.findElementByCssSelector("#doctor_chosen > a").click();
-            driver.findElementByCssSelector("#doctor_chosen > div > ul > li:nth-child(5)").click();
-            driver.findElementByCssSelector("#app > div > div.panel-body > div > div > form > div:nth-child(3) > div:nth-child(2) > div:nth-child(3) > div > div").click();
-            driver.findElementByCssSelector("#app > div > div.panel-body > div > div > form > div:nth-child(3) > div:nth-child(2) > div:nth-child(3) > div > div > div > ul > li:nth-child(3)").click();
-            driver.findElementByName("chaperone_id").click();
-            driver.findElementByCssSelector("#app > div > div.panel-body > div > div > form > div:nth-child(3) > div:nth-child(2) > div:nth-child(4) > div > select > option:nth-child(4)").click();
-            driver.findElementByCssSelector("#app > div > div.panel-body > div > div > form > div:nth-child(4) > div > div > label").click();
+            driver.findElementByXPath("//*[@id=\"doctor_chosen\"]/a/span").click();
+            driver.findElementByXPath("//*[@id=\"doctor_chosen\"]/div/ul/li[17]").click();
+            driver.findElementByXPath("//*[@id=\"app\"]/div/div[2]/div/div/form/div[2]/div[2]/div[3]/div/div/a/span").click();
             Thread.sleep(1000);
-            driver.findElementByCssSelector("#app > div > div.panel-body > div > div > form > div:nth-child(4) > div > div > div > textarea").sendKeys("autoSchedulecreate" +n);
-            driver.findElementByCssSelector("#app > div > div.panel-body > div > div > form > div:nth-child(5) > div > input").click();
-            System.out.println("Создана Шедуля врачу Athanasios Athanasiou в Bristol");
+            driver.findElementByXPath("//*[@id=\"app\"]/div/div[2]/div/div/form/div[2]/div[2]/div[3]/div/div/div/ul/li[1]").click();
+            WebElement Chaperone_drop = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div/div/form/div[2]/div[2]/div[4]/div/select"));
+            Select Chaperone_dd = new Select(Chaperone_drop);
+            Chaperone_dd.selectByValue("1");
+            driver.findElementByXPath("//*[@id=\"app\"]/div/div[2]/div/div/form/div[3]/div/div/div/textarea").sendKeys("Test Description");
+            driver.findElementByXPath("//*[@id=\"app\"]/div/div[2]/div/div/form/div[4]/div/input").click();
+            System.out.println("Создано расписание на сегодня");
 
 
     }
